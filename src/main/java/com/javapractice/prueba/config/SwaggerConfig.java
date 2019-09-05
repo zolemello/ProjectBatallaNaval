95748cc4d96f38f51f12221163879c67595fb10a
zolemello <mello.soledad@gmail.com>
2019-09-05 18:44:45 -0300

    Correccion de errores

---
+++ src/main/java/com/javapractice/prueba/config/SwaggerConfig.java
@@ -0,0 +1,102 @@
+package com.javapractice.prueba.config;
+
+
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String DEFAULT_INCLUDE_PATTERN = "/.*";
+    private final Logger log = LoggerFactory.getLogger(SwaggerConfig.class);
+
+    /*
+     * Configuracion de swagger
+     *
+     * */
+    @Bean
+    public Docket swaggerSpringfoxDocket() {
+        log.debug("Starting Swagger");
+        Contact contact = new Contact(
+                "Sebastian Bogado",
+                "http://sebastianbogado.com",
+                "seebogado@gmail.com");
+
+        List<VendorExtension> vext = new ArrayList<>();
+        ApiInfo apiInfo = new ApiInfo(
+                "Backend API Salvo",
+                "This is the best stuff since sliced bread - API",
+                "6.6.6",
+                "https://justrocket.de",
+                contact,
+                "MIT",
+                "https://justrocket.de",
+                vext);
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .apiInfo(ApiInfo.DEFAULT)
+                .forCodeGeneration(true)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .ignoredParameterTypes(Pageable.class)
+                .ignoredParameterTypes(java.sql.Date.class)
+                .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+                .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .useDefaultResponseMessages(false);
+
+        docket = docket.select()
+                .paths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+        return docket;
+    }
+
+
+
+    /*
+     * Configuro que al autenticarme, dentro del geader va a meter un header Authorization con el valor que inserte
+     * */
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+
+    /*
+     * Configuro el contexto de seguridad y le seteo que todas las apis (Debido al /*) tiene que authenticarse.
+     * */
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+}
